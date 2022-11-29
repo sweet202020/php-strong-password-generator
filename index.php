3d0cd04346a0caaf195582da55a5582730c3fc99
sweet202020 <dolcealessio1@gmail.com>
2022-11-29 14:43:07 +0100

    initial commit and add milestone 1

---
+++ index.php
@@ -0,0 +1,62 @@
+<!-- Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php -->
+
+<?php
+function generatePassword($lenghtPass)
+{
+    $characters = '.-_+*#0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $passwordString = '';
+    for ($i = 0; $i < $lenghtPass; $i++) {
+        $passwordString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $passwordString;
+}
+$passwordGenereator = isset($_GET["pass"]);
+var_dump($passwordGenereator);
+if ($passwordGenereator) {
+    echo generatePassword($_GET["pass"]);
+    # code...
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>password generator</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <h1>password generator</h1>
+        <div class="mb-3">
+            <form action="index.php" method="get">
+                <label for="" class="form-label">lunghezza password</label>
+                <input type="number" class="form-control" name="pass" id="pass" aria-describedby="helpId">
+                <button type="submit">richiedi</button>
+            </form>
+        </div>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
